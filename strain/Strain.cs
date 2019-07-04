b5ff562773b18de367059c88b294ea66b1b2887e
Joao Kita <kitajoao@gmail.com>
2019-07-04 09:17:55 -0300

    solved exercise strain 20190704

--- strain/Strain.cs
+++ strain/Strain.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Collections.Generic;
+
 public static class Strain
 {
     public static IEnumerable<T> Keep<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
-    {
-        return collection.Select(x => predicate(x));
-    }
+        => collection.Where(predicate);
 
     public static IEnumerable<T> Discard<T>(this IEnumerable<T> collection, Func<T, bool> predicate)
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+        => collection.Where(e => predicate(e) == false);
 }
