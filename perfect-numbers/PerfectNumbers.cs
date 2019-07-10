2f75616b6f08bd5b24f0eb48c487c0f229752347
Joao Kita <kitajoao@gmail.com>
2019-07-10 16:45:34 -0300

    solved exercise perferct numbers

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+        if (number <= 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+
+
+        int[] MCD = new int[number / 2];
+
+        for (int i = 1; i <= number / 2; i++)
+        {
+            if (number % i == 0)
+            {
+                MCD[i - 1] = i;
+            }
+        }
+
+        int sumMCD = 0;
+
+        for (int i = 0; i < MCD.Length; i++)
+        {
+            sumMCD = sumMCD + MCD[i];
+        }
+
+        sumMCD = sumMCD;
+
+        if (sumMCD == number)
+        {
+            return Classification.Perfect;
+        }
+        else if (sumMCD > number)
+        {
+            return Classification.Abundant;
+        }
+        else
+        {
+            return Classification.Deficient;
+        }
+    }
+}
