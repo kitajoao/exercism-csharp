e0b0818adfb3e35540eecb66d8ea78d5d128f506
Joao Kita <kitajoao@gmail.com>
2019-07-23 08:47:19 -0300

    isogram exercise

---
+++ isogram/Isogram.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        if (word == "")
+        {
+            return true;
+        }
+
+        word[1].
+        int count = 0;
+        for (int i = 0; i < word.Length; i++)
+        {
+            for (int j = 0; j < word.Length; j++)
+            {
+                if (word[i] == word[j])
+                {
+                    count++;
+                }
+            }
+        }
+        if (count > word.Length)
+        {
+            return true;
+        }
+        return false;
+    }
+}
