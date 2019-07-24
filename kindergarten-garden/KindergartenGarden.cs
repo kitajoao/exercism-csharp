4b59fb55053eb702a7abfbd897aa3365ddf36b30
Joao Kita <kitajoao@gmail.com>
2019-07-24 14:32:00 -0300

    opened exercise kindergarten-garden

---
+++ kindergarten-garden/KindergartenGarden.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Plant
+{
+    Violets,
+    Radishes,
+    Clover,
+    Grass  
+}
+
+public class KindergartenGarden
+{    
+    
+    private int plantPerStudent = 2;
+    private string thisDiagram;
+    private List<string> children = new List<string>(){"Alice", "Bob", "Charlie", "David",
+    "Eve", "Fred", "Ginny", "Harriet","Ileana", "Joseph", "Kincaid", "Larry"};
+    
+    public KindergartenGarden(string diagram)
+    {
+        thisDiagram = diagram;        
+    }
+
+    public IEnumerable<Plant> Plants(string student)
+    {
+        return thisDiagram.Split('\n').Select()
+    }
+
+    
+}
