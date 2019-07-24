9684150ecf3a761ba6530fd98ddee81ad79ea4e5
Joao Kita <kitajoao@gmail.com>
2019-07-24 13:35:44 -0300

    solved exercise secret-handshake

---
+++ secret-handshake/SecretHandshake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class SecretHandshake
+{
+    public static string[] Commands(int commandValue)
+    {
+        var stringBinary = Convert.ToString(commandValue, 2).ToCharArray().Reverse().ToArray();
+
+        List<string> answer = new List<string>();
+
+        string[] typesAnswer = new string[] { "wink", "double blink", "close your eyes", "jump" };
+
+        if (stringBinary.Length >= 4 && stringBinary[3] == '1')
+        {
+            answer.Add(typesAnswer[3]);
+        }
+        if (stringBinary.Length >= 3 && stringBinary[2] == '1')
+        {
+            answer.Add(typesAnswer[2]);
+        }
+        if (stringBinary.Length >= 2 && stringBinary[1] == '1')
+        {
+            answer.Add(typesAnswer[1]);
+        }
+
+        if (stringBinary.Length >= 1 && stringBinary[0] == '1')
+        {
+            answer.Add(typesAnswer[0]);
+        }
+        if (stringBinary.Length <= 4)
+        {
+            answer.Reverse();
+        }
+        return answer.ToArray();
+    }
+}
