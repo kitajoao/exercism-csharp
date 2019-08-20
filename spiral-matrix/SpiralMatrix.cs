2bff821f0606240c32ccd316e4f5bc64b83ed4e7
Joao Kita <kita@nfe.io>
2019-08-20 13:53:51 -0300

    solved exercise spiral-matrix

---
+++ spiral-matrix/SpiralMatrix.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class SpiralMatrix
+{
+    public static int[,] GetMatrix(int size)
+    {
+        int[,] SpiralMatrix = new int[size, size];
+        int printValue = 1;
+        int c1 = 0, c2 = size - 1;
+        while (printValue <= size * size)
+        {
+            //Right Direction Move  
+            for (int i = c1; i <= c2; i++)
+                SpiralMatrix[c1, i] = printValue++;
+            //Down Direction Move  
+            for (int j = c1 + 1; j <= c2; j++)
+                SpiralMatrix[j, c2] = printValue++;
+            //Left Direction Move  
+            for (int i = c2 - 1; i >= c1; i--)
+                SpiralMatrix[c2, i] = printValue++;
+            //Up Direction Move  
+            for (int j = c2 - 1; j >= c1 + 1; j--)
+                SpiralMatrix[j, c1] = printValue++;
+            c1++;
+            c2--;
+        }
+
+        return SpiralMatrix;
+    }
+}
