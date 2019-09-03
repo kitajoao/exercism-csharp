6cf25c52972dde49317cb5549629475e5134997b
Joao Kita <kita@nfe.io>
2019-09-03 16:39:44 -0300

    solved exercise bank account

---
+++ bank-account/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class BankAccount
+{
+    private decimal _balance;
+    private bool openAccount = true;
+    public void Open()
+    {
+        Balance = 0;
+    }
+    public void Close()
+    {
+        openAccount = false;
+
+    }
+    public decimal Balance
+    {
+        get
+        {
+            if (openAccount == false)
+                throw new InvalidOperationException();
+            return _balance;
+        }
+        set
+        {
+            _balance = value;
+        }
+    }
+    public void UpdateBalance(decimal change)
+    {
+        lock (this)
+        {
+            Balance += change;
+        }
+    }
+}
