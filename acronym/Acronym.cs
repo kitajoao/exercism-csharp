9e76353db79c85c82a7f3a6d02a4be097e2d52b6
Joao Kita <kita@nfe.io>
2019-09-03 18:27:24 -0300

    solved exercise acronym

---
+++ acronym/Acronym.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        string result = "";
+
+        phrase = phrase.ToUpper();
+        
+        phrase = phrase.Replace('-', ' ').Replace("   "," ").Replace('_', ' ').Replace("  "," ");
+
+        string[] words = phrase.Split(' ');
+
+        foreach (var word in words)
+        {
+            result += word[0];
+        }
+        return result;
+    }
+}
