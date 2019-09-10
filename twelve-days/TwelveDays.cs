f5b3d8a048ffa27f664554bae56107ad797ef697
Joao Kita <kita@nfe.io>
2019-09-10 15:25:20 -0300

    solved exercise twelve-days

---
+++ twelve-days/TwelveDays.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+public static class TwelveDays
+{
+    public static string[] days = new string []{"On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.", 
+
+"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.",  
+
+"On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.", 
+
+"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."};
+
+
+    public static string Recite(int verseNumber)
+    {
+        return days[verseNumber - 1];
+    }
+
+    public static string Recite(int startVerse, int endVerse)
+    {
+        string answer = "";
+        {
+            for (int i = (startVerse -1); i < endVerse; i++)
+            {
+                answer += days[i];
+
+                if(i == (endVerse - 1))
+                {
+                    break;
+                }
+                answer += "\n";
+            }
+            return answer;
+        }
+    }
+}
+
+
+
