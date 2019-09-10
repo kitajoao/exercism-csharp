c5b3d6cf1b97759665e1839bee1037fc9035d689
Joao Kita <kita@nfe.io>
2019-09-10 14:54:00 -0300

    solved exercise pythagorean triplet

---
+++ pythagorean-triplet/PythagoreanTriplet.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public static class PythagoreanTriplet
+{
+    public static IEnumerable<(int a, int b, int c)> TripletsWithSum(int sum)
+    {
+        var answer = new List<(int, int, int)>();
+
+        bool sumOfSides = false;
+
+        for (int ai = 1; ai < sum / 3; ai++)
+        {
+            for (int bi = ai; bi < sum / 2; bi++)
+            {
+                int ci = sum - ai - bi;
+
+                if ((ai * ai) + (bi * bi) == (ci * ci))
+                {
+                    answer.Add((ai, bi, ci));
+                }
+            }
+        }
+        return answer.ToArray();
+    }
+}
