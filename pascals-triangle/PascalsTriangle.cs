45ed5b85ad465b37fdfa237db3bb4e1adfa6c4d7
Joao Kita <kita@nfe.io>
2019-09-13 19:12:00 -0300

    solved exercise pascal triangle

---
+++ pascals-triangle/PascalsTriangle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class PascalsTriangle
+{
+    public static IEnumerable<IEnumerable<int>> Calculate(int rows)
+    {
+        var arrayList = new List<int[]>();
+        var count = 0;
+        var a = new int[count];
+        for (int i = 1; i <= rows; i++)
+        {
+            var x = new int[i];
+            for (int j = 0; j < i; j++)
+            {
+                if (j == 0 || j == i - 1)
+                {
+                    x[j] = 1;
+                }
+                else
+                    x[j] += a[j] + a[j - 1];
+            }
+            count++;
+            a = x;
+            arrayList.Add(x);
+        }
+        return arrayList.ToArray();
+    }
+
+}
