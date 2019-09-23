19eb24aeae35b4e5ff69679610624d5500ad6875
Joao Kita <kita@nfe.io>
2019-09-23 15:03:03 -0300

    solved exercise flatten array

---
+++ flatten-array/FlattenArray.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+public static class FlattenArray
+{
+    public static IEnumerable Flatten(IEnumerable input)
+    {
+        var finalArray = new List<int>();
+
+        foreach (var item in input)
+        {
+            if(item == null)
+            {
+                continue;
+            }
+            if (item.GetType() == typeof(int))
+            {
+                finalArray.Add(Convert.ToInt32(item));
+            }
+            else if (item.GetType() == typeof(object[]))
+            {
+                IteratorObject(finalArray, item as object[]);
+            }
+        }
+        return finalArray.ToArray();
+    }
+
+    public static List<int> IteratorObject(List<int> arr, object[] data)
+    {
+        foreach(var item in data)
+        {
+            if(item == null)
+            {
+                continue;
+            }
+            if(item.GetType() == typeof(int))
+            {
+                arr.Add(Convert.ToInt32(item));
+            }
+            else if (item.GetType() == typeof(object[]))
+            {
+                IteratorObject(arr, item as object[]);
+            }
+        }
+        return arr;
+    }
+}
