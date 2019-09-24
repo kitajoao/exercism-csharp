8d2f321db1a135d52ef08ec70a17d1708d618320
Joao Kita <kita@nfe.io>
2019-09-24 15:31:06 -0300

    reviewed exercise parallel letter frequency

--- parallel-letter-frequency/ParallelLetterFrequency.cs
+++ parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -5,24 +5,10 @@ public static class ParallelLetterFrequency
 {
     public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
     {
-        var qtyOfChars = new Dictionary<char, int>();
-
-        var list = texts.ToList().Select(x=> x.ToLower().Select(y => y))
-                        .SelectMany(x => x.Where(y => Char.IsLetter(y)));
-        
-        foreach (var item in list)
-        {
-            if (qtyOfChars.ContainsKey(item))
-            {
-                qtyOfChars[item]++;
-            }
-            else
-            {
-                qtyOfChars.Add(item, 1);
-            }
-
-        }
-
-        return qtyOfChars;
+        return texts
+            .SelectMany(x => x.ToLowerInvariant().Where(y => Char.IsLetter(y)))
+            .GroupBy(e => e)
+            .Select(group => new { Char = group.Key, Count = group.Count()})
+            .ToDictionary(e => e.Char, e => e.Count);
     }
 }
