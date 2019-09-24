55f39d25650f565e1cc609e3a77426bfdb089907
Joao Kita <kita@nfe.io>
2019-09-24 15:17:00 -0300

    solved exercise parallel letter frequency

---
+++ parallel-letter-frequency/ParallelLetterFrequency.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+public static class ParallelLetterFrequency
+{
+    public static Dictionary<char, int> Calculate(IEnumerable<string> texts)
+    {
+        var qtyOfChars = new Dictionary<char, int>();
+
+        var list = texts.ToList().Select(x=> x.ToLower().Select(y => y))
+                        .SelectMany(x => x.Where(y => Char.IsLetter(y)));
+        
+        foreach (var item in list)
+        {
+            if (qtyOfChars.ContainsKey(item))
+            {
+                qtyOfChars[item]++;
+            }
+            else
+            {
+                qtyOfChars.Add(item, 1);
+            }
+
+        }
+
+        return qtyOfChars;
+    }
+}
