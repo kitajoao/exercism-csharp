1ae87ff4ef02d1063460b06fbfaad61c58002159
Joao Kita <kita@nfe.io>
2019-09-24 08:31:02 -0300

    solved exercise binary search tree

--- binary-search-tree/BinarySearchTree.cs
+++ binary-search-tree/BinarySearchTree.cs
@@ -1,53 +1,73 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-
+using System.Linq;
 public class BinarySearchTree : IEnumerable<int>
 {
     public BinarySearchTree(int value)
     {
+        Value = value;
     }
 
     public BinarySearchTree(IEnumerable<int> values)
     {
+        var listOfValues = new List<int>(values);
+        this.Value = listOfValues[0];
+        for (int i = 1; i < listOfValues.Count; i++)
+        {
+            this.Add(listOfValues[i]);
+        }            
     }
 
     public int Value
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        get;
+        set;
     }
 
     public BinarySearchTree Left
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        get;
+        set;
     }
 
     public BinarySearchTree Right
     {
-        get
-        {
-            throw new NotImplementedException("You need to implement this function.");
-        }
+        get;
+        set;
     }
 
     public BinarySearchTree Add(int value)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (value <= Value)
+        {
+           if (Left == null)
+               return (Left = new BinarySearchTree(value));
+           else
+            return (Left.Add(value));
+        }
+        else
+        {
+           if (Right == null)
+               return (Right = new BinarySearchTree(value));
+           else // must be > right
+            return (Right.Add(value));
+        }
     }
 
     public IEnumerator<int> GetEnumerator()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if (Left != null)
+            foreach (var l in Left)
+                yield return l;
+        yield return Value;
+        if (Right != null)
+            foreach (var r in Right)
+                yield return r;
     }
 
     IEnumerator IEnumerable.GetEnumerator()
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return GetEnumerator();
     }
 }
