4e1d6ad61304a03dc272d48c313d8f2e8ad7d06c
Joao Kita <kita@nfe.io>
2019-09-24 08:31:45 -0300

    started solving exercise

---
+++ minesweeper/Minesweeper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public static class Minesweeper
+{
+    public static string[] Annotate(string[] input)
+    {
+        string[] returnMine = input;
+        
+        string[] charMatrix = input.;
+
+        int numbLines = input.Length;
+        int numbCols = input[0].Length;
+        
+        var stringMineField = new string[numbLines, numbCols];
+        var intMineField = new int[numbLines, numbCols];
+
+        // for (int i = 0; i < numbLines; i++)
+        // {
+        //     charMatrix.Add(input[i].Split("").ToArray());
+        // }
+
+        Console.WriteLine(numbLines);
+        Console.WriteLine(numbCols);
+
+        return returnMine;
+    }
+}
---
+++ minesweeper/MinesweeperTest.cs
@@ -0,0 +1,231 @@
+// This file was auto-generated based on version 1.1.0 of the canonical data.
+//dotnet add package XunitLogger --version 2.4.0
+
+using Xunit;
+using System;
+using Xunit.Abstractions;
+
+public class MinesweeperTest : IDisposable
+{
+    public MinesweeperTest(ITestOutputHelper testOutput)
+    {
+        XunitLogging.Register(testOutput);
+    }
+    public void Dispose()
+    {
+        XunitLogging.Flush();
+    }
+
+
+    [Fact]
+    public void No_rows()
+    {
+        var minefield = Array.Empty<string>();
+        var expected = Array.Empty<string>();
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact]
+    public void No_columns()
+    {
+        var minefield = new[]
+        {
+            ""
+        };
+        var expected = new[]
+        {
+            ""
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact]
+    public void No_mines()
+    {
+        var minefield = new[]
+        {
+            "   ",
+            "   ",
+            "   "
+        };
+        var expected = new[]
+        {
+            "   ",
+            "   ",
+            "   "
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact]
+    public void Minefield_with_only_mines()
+    {
+        var minefield = new[]
+        {
+            "***",
+            "***",
+            "***"
+        };
+        var expected = new[]
+        {
+            "***",
+            "***",
+            "***"
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Mine_surrounded_by_spaces()
+    {
+        var minefield = new[]
+        {
+            "   ",
+            " * ",
+            "   "
+        };
+        var expected = new[]
+        {
+            "111",
+            "1*1",
+            "111"
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Space_surrounded_by_mines()
+    {
+        var minefield = new[]
+        {
+            "***",
+            "* *",
+            "***"
+        };
+        var expected = new[]
+        {
+            "***",
+            "*8*",
+            "***"
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Horizontal_line()
+    {
+        var minefield = new[]
+        {
+            " * * "
+        };
+        var expected = new[]
+        {
+            "1*2*1"
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Horizontal_line_mines_at_edges()
+    {
+        var minefield = new[]
+        {
+            "*   *"
+        };
+        var expected = new[]
+        {
+            "*1 1*"
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Vertical_line()
+    {
+        var minefield = new[]
+        {
+            " ",
+            "*",
+            " ",
+            "*",
+            " "
+        };
+        var expected = new[]
+        {
+            "1",
+            "*",
+            "2",
+            "*",
+            "1"
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Vertical_line_mines_at_edges()
+    {
+        var minefield = new[]
+        {
+            "*",
+            " ",
+            " ",
+            " ",
+            "*"
+        };
+        var expected = new[]
+        {
+            "*",
+            "1",
+            " ",
+            "1",
+            "*"
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Cross()
+    {
+        var minefield = new[]
+        {
+            "  *  ",
+            "  *  ",
+            "*****",
+            "  *  ",
+            "  *  "
+        };
+        var expected = new[]
+        {
+            " 2*2 ",
+            "25*52",
+            "*****",
+            "25*52",
+            " 2*2 "
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+
+    [Fact(Skip = "Remove to run test")]
+    public void Large_minefield()
+    {
+        var minefield = new[]
+        {
+            " *  * ",
+            "  *   ",
+            "    * ",
+            "   * *",
+            " *  * ",
+            "      "
+        };
+        var expected = new[]
+        {
+            "1*22*1",
+            "12*322",
+            " 123*2",
+            "112*4*",
+            "1*22*2",
+            "111111"
+        };
+        Assert.Equal(expected, Minesweeper.Annotate(minefield));
+    }
+}
