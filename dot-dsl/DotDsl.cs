110d60c3d3f17d5099d6609201f26f2e934cf905
Joao Kita <kita@nfe.io>
2019-10-09 17:07:57 -0300

    viewed xercise dot-dsl

---
+++ dot-dsl/DotDsl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+
+public class Node
+{
+}
+
+public class Edge
+{
+}
+
+public class Attr
+{
+}
+
+public class Graph
+{
+}
