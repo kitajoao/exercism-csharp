6cd91d6b198df027efa665b41ae4a71dbfb782fe
hackers <hackers@nfe.io>
2020-03-30 20:32:45 -0300

    solved exercise RunLengthEncoding

--- run-length-encoding/RunLengthEncoding.cs
+++ run-length-encoding/RunLengthEncoding.cs
@@ -3,12 +3,76 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 
-
 public static class RunLengthEncoding
 {
     public static string Encode(string input)
     {
+        if (input.Length == 0)
+            return input;
 
+        var ret = "";
+        var countChar = 1;
+        char lastChar = default(char);
+
+        foreach (var chr in input)
+        {
+            if (lastChar == chr)
+            {
+                countChar++;
+            }
+            else if (lastChar != default(char) && lastChar != chr)
+            {
+                if (countChar > 1)
+                    ret += countChar.ToString() + lastChar;
+                else 
+                    ret += lastChar;
+
+                countChar = 1;
+            }
+
+            lastChar = chr;            
+        }
+
+        if (countChar > 1)
+            ret += countChar.ToString() + lastChar;
+        else 
+            ret += lastChar;
+
+        return ret;
+
+        // var ret = new List<(char, int)>();
+        // var pos = 0;
+        // char lastChar = default(char);
+
+        // foreach (var chr in input)
+        // {
+        //     if (lastChar != chr)
+        //     {
+        //         ret.Add((chr, 1));
+        //     }
+        //     else if (lastChar == chr)
+        //     {
+        //         var tuple = ret[ret.Count - 1];
+        //         tuple.Item2 += 1;
+        //         ret[ret.Count - 1] = tuple;
+        //     }
+
+        //     lastChar = chr;
+        //     pos++;
+        // }
+
+        // return ret
+        //     .Select(t => { 
+        //         if (t.Item2 > 1)
+        //             return t.Item2.ToString() + t.Item1.ToString();
+        //         else 
+        //             return t.Item1.ToString();
+        //     })
+        //     .Aggregate((a,b) => a + b);
+    }
+
+    public static string _Encode(string input)
+    {
         if (input.Length == 0)
         {
             return input;
@@ -57,7 +121,25 @@ public static class RunLengthEncoding
     }
     public static string Decode(string input)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var dgt = default(string);
+        var ret = "";
+
+        foreach (var chr in input)
+        {
+            if (char.IsDigit(chr))
+            {
+                dgt += chr;
+            }
+            else
+            {
+                var dgtCount = 1;
+                int.TryParse(dgt, out dgtCount);
+                dgt = default(string);
+                ret += chr.ToString().PadRight(dgtCount, chr);
+            }
+        }
+
+        return ret;
     }
 }
 
