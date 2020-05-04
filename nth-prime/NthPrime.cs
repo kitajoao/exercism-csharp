84122ac5d21f9c8f4d91360e1c6b6d287170aa55
hackers <hackers@nfe.io>
2020-05-04 16:53:38 -0300

    solved exercise nth-prime

--- nth-prime/NthPrime.cs
+++ nth-prime/NthPrime.cs
@@ -6,6 +6,50 @@ public static class NthPrime
 {
     public static int Prime(int nth)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        List<int> primePositions = new List<int>();
+        List<int> primeNumb = new List<int>();
+        int actualPrimePos = 0;
+        int actualPrimeNumb = 1;
+        int numb = 2;
+
+        int primeCount = 0;
+
+        if(nth == 0)
+        {
+            throw new ArgumentOutOfRangeException();
+        }
+    
+        while (actualPrimePos != nth)
+        {
+            var it = 1;
+            for (int i = 2; i <= numb; i++)
+            {
+                if (numb % i == 0)
+                {
+                    if (numb == i && it == 1)
+                    {
+                        primeCount++;
+                        primeNumb.Add(i);
+                        primePositions.Add(primeCount);
+                        break;
+                    }
+                    else
+                    {
+                        it++;
+                    }
+
+                    continue;                
+                }
+            }
+            actualPrimeNumb = primeNumb.Last();
+            actualPrimePos = primePositions.Last();
+            numb++;
+        }
+        return actualPrimeNumb;
     }
+
+
+
+
+    
 }
--- nth-prime/NthPrimeTests.cs
+++ nth-prime/NthPrimeTests.cs
@@ -1,35 +1,46 @@
 // This file was auto-generated based on version 2.1.0 of the canonical data.
 
+//dotnet add package XunitLogger --version 2.4.0
+
+using System.Collections.Generic;
 using System;
 using Xunit;
-
-public class NthPrimeTests
+using Xunit.Abstractions;
+public class NthPrimeTests :  IDisposable
 {
+    public NthPrimeTests(ITestOutputHelper testOutput)
+    {
+        XunitLogging.Register(testOutput);
+    }
+    public void Dispose()
+    {
+        XunitLogging.Flush();
+    }
     [Fact]
     public void First_prime()
     {
         Assert.Equal(2, NthPrime.Prime(1));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Second_prime()
     {
         Assert.Equal(3, NthPrime.Prime(2));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Sixth_prime()
     {
         Assert.Equal(13, NthPrime.Prime(6));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Big_prime()
     {
         Assert.Equal(104743, NthPrime.Prime(10001));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void There_is_no_zeroth_prime()
     {
         Assert.Throws<ArgumentOutOfRangeException>(() => NthPrime.Prime(0));
