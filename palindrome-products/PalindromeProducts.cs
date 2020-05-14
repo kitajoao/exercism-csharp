5e829e6acf46d109d4537d7e2b4327f4bc10e379
hackers <hackers@nfe.io>
2020-05-14 18:59:29 -0300

    solved exercise palindrome-products

--- palindrome-products/PalindromeProducts.cs
+++ palindrome-products/PalindromeProducts.cs
@@ -2,15 +2,105 @@
 using System.Collections.Generic;
 using System.Linq;
 
+
 public static class PalindromeProducts
 {
-    public static (int, IEnumerable<(int,int)>) Largest(int minFactor, int maxFactor)
+
+    private static List<Tuple<int, int, int>> CheckPalindrome(List<Tuple<int, int, int>> palindromeList, bool highest = true)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        
+        var orderedProduct = highest? palindromeList.OrderByDescending(p => p.Item3) : palindromeList.OrderBy(p => p.Item3);
+        
+        var allPalindromeNumb = new List<Tuple<int, int, int>>();
+        foreach (var memb in orderedProduct)
+        {
+            int reminder, sum = 0;
+            int numb = memb.Item3;
+
+            while (numb > 0)
+            {
+                reminder = numb % 10;
+                sum = (sum * 10) + reminder;
+                numb = numb / 10;
+            }
+            if (memb.Item3 == sum || ((memb.Item1 < 10 && memb.Item2 < 10 && memb.Item3 < 10) && (memb.Item1 * memb.Item2 == memb.Item3)))
+            {
+                allPalindromeNumb.Add(new Tuple< int, int, int>( memb.Item1, memb.Item2, memb.Item3));
+            }
+        }
+        return allPalindromeNumb;
+    }
+    public static (int, IEnumerable<(int, int)>) Largest(int minFactor, int maxFactor)
+    {
+        if(maxFactor - minFactor <= 1)
+        {
+            throw new ArgumentException();
+        }
+        
+        List<Tuple<int, int, int>> possibleProducts = new List<Tuple<int, int, int>>();
+
+        //create all possible products combination
+        for (int i = maxFactor; i >= minFactor; i--)
+        {
+            int j;
+            for (j = i; j >= minFactor; j--)
+            {
+                possibleProducts.Add(new Tuple< int, int, int>(i, j, i * j));
+            }
+        }
+
+        //find all palindrome numbers
+        var allPalindromeNumb = CheckPalindrome(possibleProducts, true);
+
+        //find the highest palindrome
+        var highestPalindromeNumb = allPalindromeNumb.Select(p => p.Item3).First();
+        var result = new List<(int, int)>();
+
+        foreach (var item in allPalindromeNumb)
+        {
+            if (item.Item3 == highestPalindromeNumb)
+            {
+                result.Add((item.Item2, item.Item1));
+            }
+        }
+
+        return (highestPalindromeNumb, result);
     }
 
-    public static (int, IEnumerable<(int,int)>) Smallest(int minFactor, int maxFactor)
+    public static (int, IEnumerable<(int, int)>) Smallest(int minFactor, int maxFactor)
     {
-        throw new NotImplementedException("You need to implement this function.");
+        if(maxFactor - minFactor <= 1)
+        {
+            throw new ArgumentException();
+        }
+       List<Tuple<int, int, int>> possibleProducts = new List<Tuple<int, int, int>>();
+
+        //create all possible products combination
+        for (int i = maxFactor; i >= minFactor; i--)
+        {
+            int j;
+            for (j = i; j >= minFactor; j--)
+            {
+                possibleProducts.Add(new Tuple< int, int, int>(i, j, i * j));
+            }
+        }
+
+        //find all palindrome numbers
+        var allPalindromeNumb = CheckPalindrome(possibleProducts, false);
+
+        // find lowest palindrome
+        var smallestPalindromeNumb = allPalindromeNumb.Select(p => p.Item3).First();
+        var result = new List<(int, int)>();
+
+        foreach (var item in allPalindromeNumb)
+        {
+            if (item.Item3 == smallestPalindromeNumb)
+            {
+                result.Add((item.Item2, item.Item1));
+            }
+        }
+
+        return (smallestPalindromeNumb, result);
     }
 }
+
--- palindrome-products/PalindromeProductsTests.cs
+++ palindrome-products/PalindromeProductsTests.cs
@@ -1,10 +1,21 @@
 // This file was auto-generated based on version 1.2.0 of the canonical data.
+//dotnet add package XunitLogger --version 2.4.0
 
+using System.Collections.Generic;
 using System;
 using Xunit;
-
-public class PalindromeProductsTests
+using Xunit.Abstractions;
+public class PalindromeProductsTests :  IDisposable
 {
+    public PalindromeProductsTests(ITestOutputHelper testOutput)
+    {
+        XunitLogging.Register(testOutput);
+    }
+    public void Dispose()
+    {
+        XunitLogging.Flush();
+    }
+
     [Fact]
     public void Finds_the_smallest_palindrome_from_single_digit_factors()
     {
@@ -14,7 +25,7 @@ public class PalindromeProductsTests
         Assert.Equal(expected.Item2, actual.Item2);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Finds_the_largest_palindrome_from_single_digit_factors()
     {
         var actual = PalindromeProducts.Largest(1, 9);
@@ -23,7 +34,7 @@ public class PalindromeProductsTests
         Assert.Equal(expected.Item2, actual.Item2);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Find_the_smallest_palindrome_from_double_digit_factors()
     {
         var actual = PalindromeProducts.Smallest(10, 99);
@@ -32,7 +43,7 @@ public class PalindromeProductsTests
         Assert.Equal(expected.Item2, actual.Item2);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Find_the_largest_palindrome_from_double_digit_factors()
     {
         var actual = PalindromeProducts.Largest(10, 99);
@@ -41,7 +52,7 @@ public class PalindromeProductsTests
         Assert.Equal(expected.Item2, actual.Item2);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Find_smallest_palindrome_from_triple_digit_factors()
     {
         var actual = PalindromeProducts.Smallest(100, 999);
@@ -50,7 +61,7 @@ public class PalindromeProductsTests
         Assert.Equal(expected.Item2, actual.Item2);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Find_the_largest_palindrome_from_triple_digit_factors()
     {
         var actual = PalindromeProducts.Largest(100, 999);
@@ -59,7 +70,7 @@ public class PalindromeProductsTests
         Assert.Equal(expected.Item2, actual.Item2);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Find_smallest_palindrome_from_four_digit_factors()
     {
         var actual = PalindromeProducts.Smallest(1000, 9999);
@@ -68,7 +79,7 @@ public class PalindromeProductsTests
         Assert.Equal(expected.Item2, actual.Item2);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Find_the_largest_palindrome_from_four_digit_factors()
     {
         var actual = PalindromeProducts.Largest(1000, 9999);
@@ -77,25 +88,25 @@ public class PalindromeProductsTests
         Assert.Equal(expected.Item2, actual.Item2);
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Empty_result_for_smallest_if_no_palindrome_in_the_range()
     {
         Assert.Throws<ArgumentException>(() => PalindromeProducts.Smallest(1002, 1003));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Empty_result_for_largest_if_no_palindrome_in_the_range()
     {
         Assert.Throws<ArgumentException>(() => PalindromeProducts.Largest(15, 15));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Error_result_for_smallest_if_min_is_more_than_max()
     {
         Assert.Throws<ArgumentException>(() => PalindromeProducts.Smallest(10000, 1));
     }
 
-    [Fact(Skip = "Remove this Skip property to run this test")]
+    [Fact]
     public void Error_result_for_largest_if_min_is_more_than_max()
     {
         Assert.Throws<ArgumentException>(() => PalindromeProducts.Largest(2, 1));
